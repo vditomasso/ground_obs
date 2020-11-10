9a079eba53e5532ea34eaf87e145aa9d51f642e3
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-11-10 17:02:37 -0500

    add get_R method and add R as an attribute, calculated and added at init

--- ground_obs/spec_obj.py
+++ ground_obs/spec_obj.py
@@ -24,6 +24,7 @@ class Spectrum():
         '''
         self.wav = wav
         self.flux = flux
+        self.R = self.get_R()
         self.wav_range = np.array([min(wav),max(wav)])
         self.wav_unit = u.Unit(wav_unit)
         try:
@@ -31,9 +32,18 @@ class Spectrum():
         except ValueError:
             print('{} is not an astropy unit. \n Maybe you would like cm, micron, or nm?'.format(wav_unit))
             
-    def to(self,new_unit):
+    def get_R(self):
+        '''Find the resolution of the Spectrum'''
+        diffs = np.diff(self.wav)  # Calculates Delta lambdas
+        diffs = np.append(diffs, diffs[-1])  # Keeps len(diffs) == len(wavs)
+        return self.wav / diffs  # R = lambda / Delta lambda
+            
+    def to_unit(self,new_unit):
         '''Update the wavelength unit of the Spectrum object'''
         self.wav_unit = u.Unit(new_unit)
+        
+#    def to_range(self, min_wav, max_wav):
+#        '''Restrict the range of the wa'''
 
 ### Testing ###
 import data_io
@@ -42,10 +52,11 @@ exo_spec_file = 'O2_1E6.txt'
 
 tel_spec_df, exo_spec_df = data_io.load_data(data_io.get_data_file_path(telluric_spec_file), data_io.get_data_file_path(exo_spec_file))
 
-wav = tel_spec_df['wavelength']
-flux = tel_spec_df['flux']
+wav = np.array(tel_spec_df['wavelength'])
+flux = np.array(tel_spec_df['flux'])
 
 test_spec = Spectrum(wav, flux, 'micron')
-print(test_spec.wav_unit)
-test_spec.to('cm')
-print(test_spec.wav_unit)
+#print(test_spec.wav_unit)
+#test_spec.to_unit('cm')
+#print(test_spec.wav_unit)
+print(test_spec.R)
