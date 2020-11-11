2eec6b9169e1d98c6ee1cd6e75bb47bc8459e02a
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-11-11 10:27:19 -0500

    edit error handling in __init__, __str__, to_air and to_vac

--- ground_obs/spec_obj.py
+++ ground_obs/spec_obj.py
@@ -16,27 +16,32 @@ from spectres import spectres
 
 class Spectrum():
 
-    def __init__(self,wav,flux,wav_unit,medium):
+    def __init__(self,wav,flux,wav_unit,medium='vac'):
         '''
         Initialize a Spectrum object
         wav: wavelength (array)
         flux: flux (array)
         wav_unit: wavelength unit (string)
-        medium: either air or vac (string)
+        medium: either 'vac' or 'air' (string), default: 'vac'
         '''
-        self.wav = wav
-        self.flux = flux
-        self.R = self._R()
-        self.wav_range = np.array([min(wav),max(wav)])
-        self.wav_unit = u.Unit(wav_unit)
-        # Could add check that wavelength array is increasing
         try:
+            self.wav = wav
+            self.flux = flux
+            self.R = self._R()
+            self.wav_range = np.array([min(wav),max(wav)])
             self.wav_unit = u.Unit(wav_unit)
+            assert (medium =='vac') or (medium == 'air')
+            self.medium = medium
         except ValueError:
-            print('{} is not an astropy unit. \n Maybe you would like cm, micron, or nm?'.format(wav_unit))
-            
+            print('ValueError: {} is not an astropy unit. \n Maybe you would like cm, micron, or nm?'.format(wav_unit))
+        except AssertionError:
+            print('AssertionError: Medium needs to be either vac or air, not {}'.format(medium))
+
     def __str__(self):
-        return('Spectrum object with wav_range = {}-{} {} and avg R = {}'.format(np.round(self.wav_range[0]),np.round(self.wav_range[1]),self.wav_unit,np.round(np.mean(self.R))))
+        try:
+            return('Spectrum object with {} wavelength range = {}-{} {} and avg R = {}'.format(self.medium,np.round(self.wav_range[0]),np.round(self.wav_range[1]),self.wav_unit,np.round(np.mean(self.R))))
+        except AttributeError:
+            return('Spectrum object was not initiated properly. \n Check that it has attributes: wav, flux, wav_range, R and medium.')
             
     def _R(self):
         '''Find the resolution of the Spectrum'''
@@ -44,27 +49,35 @@ class Spectrum():
         diffs = np.append(diffs, diffs[-1])  # Keeps len(diffs) == len(wavs)
         return self.wav / diffs  # R = lambda / Delta lambda
         
-    def vac2air(self):
+    def to_air(self):
     # Following VALD3: http://www.astro.uu.se/valdwiki/Air-to-vacuum%20conversion
+        R_before = self._R()
         if self.medium == 'air':
-            return('Spectrum is already in air wavelengths')
+            print('Spectrum is already in air wavelengths')
         elif self.medium == 'vac':
             s = 10**4/self.wav
             n = 1 + 0.0000834254 + 0.02406147 / (130-s**2) + 0.00015998 / (38.9-s**2)
             self.wav = self.wav / n
+            self.wav_range = np.array([min(self.wav),max(self.wav)])
             self.medium = 'air'
+#            ### Maybe this should become a test in the testing module
+#            if self._R().any()!=R_before.any():
+#                print('Error: Resolution of the spectrum has changed')
         else:
-            return('self.medium is neither air nor vac')
+            print('Error: self.medium is neither air nor vac')
             
-    def air2vac(self):
+    def to_vac(self):
     # Following VALD3: http://www.astro.uu.se/valdwiki/Air-to-vacuum%20conversion
         if self.medium=='vac':
-            return('Spectrum is already in vacuum wavelengths')
+            print('Spectrum is already in vacuum wavelengths')
         elif self.medium == 'air':
             s = 10**4/self.wav
             n =1 + 0.00008336624212083 + 0.02408926869968 / (130.1065924522-s**2) + 0.0001599740894897 / (38.92568793293-s**2)
             self.wav = self.wav*n
+            self.wav_range = np.array([min(self.wav),max(self.wav)])
             self.medium = 'vac'
+        else:
+            print('Error: self.medium is neither air nor vac')
 
     def to_unit(self,new_unit):
         '''Update the wavelength unit of the Spectrum instance'''
@@ -80,13 +93,12 @@ class Spectrum():
             self.wav = self.wav[indices]
             self.flux = self.flux[indices]
         else:
-            print('The input wavelength range ({}-{}) is not a subset of the initial wavelength range of this spectrum'.format(wav_min,wav_max))
+            print('Error: The input wavelength range ({}-{}) is not a subset of the initial wavelength range of this spectrum'.format(wav_min,wav_max))
 
     def change_R(self,R):
         '''Change the resolution of the spetrum -- updates the wav, flux and R attributes'''
         # Adapted from Ian's O2/utils.py resample function
         ### ADD IN A CHECK THAT THE RESULTING SELF._R() ACTUALLY MATCHES THE INPUT R
-        print('len(self.wav) before = ',len(self.wav))
         wav_min, wav_max = self.wav_range
         wav_central = (wav_min + wav_max) / 2
         wav_delta = wav_central / R
@@ -97,7 +109,7 @@ class Spectrum():
             self.flux = flux_resampled
             self.R = np.ones(self.wav.shape)*R
         else:
-            print('Spectrum wav_range has changed')
+            print('Error: Spectrum wav_range has changed')
 
 
 ### Testing ###
@@ -111,9 +123,11 @@ wav = np.array(tel_spec_df['wavelength'])
 flux = np.array(tel_spec_df['flux'])
 
 test_spec = Spectrum(wav, flux, 'nm')
-print(test_spec)
+#print(test_spec)
 test_spec.change_wav_range(750,780)
 print(test_spec)
-test_spec.change_R(3e5)
+#test_spec.change_R(3e5)
+#print(type(test_spec.medium))
+test_spec.to_air()
 print(test_spec)
 
